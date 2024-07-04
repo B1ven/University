e569b184b01e8de7ded2ffd447a63b0a019ea88d
Siabram1 <146183763+B1ven@users.noreply.github.com>
2024-07-04 22:18:24 +0300

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -1,29 +1,41 @@
+"""Задача "Рассылка писем": Часто при разработке и работе с рассылками писем(e-mail) они отправляются от одного и
+того же пользователя(администрации или службы поддержки). Тем не менее должна быть возможность сменить его в редких
+случаях. Попробуем реализовать функцию с подробной логикой.
+
+Создайте функцию send_email, которая принимает 2 обычных аргумента: сообщение и получатель и 1 обязательно
+именованный аргумент со значением по умолчанию - отправитель. Внутри функции реализовать следующую логику:
+
+    Проверка на корректность e-mail отправителя и получателя.
+    Проверка на отправку самому себе.
+    Проверка на отправителя по умолчанию.
+
+Пункты задачи:
+
+    Создайте функцию send_email, которая принимает 2 обычных аргумента: message(сообщение), recipient(получатель) и 1
+    обязательно именованный аргумент со значением по умолчанию sender = "university.help@gmail.com". Если строки
+    recipient и sender не содержит "@" или не оканчивается на ".com"/".ru"/".net", то вывести на экран(в консоль)
+    строку: "Невозможно отправить письмо с адреса <sender> на адрес <recipient>". Если же sender и recipient
+    совпадают, то вывести "Нельзя отправить письмо самому себе!" Если же отправитель по умолчанию -
+    university.help@gmail.com, то вывести сообщение: "Письмо успешно отправлено с адреса <sender> на адрес
+    <recipient>." В противном случае вывести сообщение: "НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса
+    <sender> на адрес <recipient>." Здесь <sender> и <recipient> - значения хранящиеся в этих переменных. За один
+    вызов функции выводится только одно и перечисленных уведомлений! Проверки перечислены по мере выполнения."""
+
+
 def send_email(message, recipient, sender="university.help@gmail.com"):
-    correct_domen = ('.ru', '.com', '.net')
-    valid_end = False
-    
-    for c in correct_domen:
-        if c in recipient:
-            valid_end = True
-            break
-    for c in correct_domen:
-        if c in sender:
-            valid_end = True
-            break
+    correct_domen = ('.com', '.ru', '.net')
+    valid = recipient.endswith(correct_domen, -4) == sender.endswith(correct_domen, -4)
+    if recipient.count('@') == 1 and sender.count('@') == 1:
+        if recipient not in '@' and valid is not True:
+            print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+        elif recipient == sender:
+            print('Нельзя отправить письмо самому себе!')
+        elif sender == 'university.help@gmail.com':
+            print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
         else:
-            valid_end = False
-    
-    
-    if recipient not in "@" and valid_end == False:
-        print("Невозможно отправить письмо с адреса {} на адрес {}".format(sender, recipient))
-    elif recipient == sender:
-        print('Нельзя отправить письмо самому себе!')
-    elif sender == "university.help@gmail.com":
-        print('Письмо успешно отправлено с адреса {} на адрес {}'.format(sender, recipient))
+            print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
     else:
-        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {} на адрес {}'.format(sender, recipient))
-    
-        
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
 
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
