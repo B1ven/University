3697ce09d60f2cf7cb05d238909c86cc5d5d5899
Siabram1 <146183763+B1ven@users.noreply.github.com>
2024-07-17 22:56:41 +0300

    Модули и пакеты

---
+++ fake_math.py
@@ -0,0 +1,2 @@
+def fake_divide(x, y):
+    return x / y if y != 0 and x != 0 else 'Ошибка'
---
+++ true_math.py
@@ -0,0 +1,6 @@
+from math import inf
+
+
+def true_divide(x, y):
+    return x / y if y != 0 and x != 0 else inf
+
