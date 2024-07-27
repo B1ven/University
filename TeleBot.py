eadc3adae82a3ca22ebf2a8a36d8e30d43b566b7
Siabram1 <146183763+B1ven@users.noreply.github.com>
2024-07-27 15:49:05 +0300

    Update TeleBot.py

--- TeleBot.py
+++ TeleBot.py
@@ -14,12 +14,21 @@ def check(m):
     return False
 
 
+def check_valid_poll(x):
+    count = x.count("\n")
+    if 3 < count <= 11:
+        return True
+    return False
+
+
 @bot.message_handler(commands=['start'])
 def say_hello(message):
     bot.send_message(message.chat.id, 'Приветствую!')
     text = f'''Твой ник {message.from_user.username}\n
     Я умею складывать твой текст 10 раз\n
     Но! Если увижу слово рандом могу выдать и число\n
+    ОБНОВЛЕНИЕ!!!\n
+    Теперь могу создавать опросы если увижу больше 3-х строк!\n
     Можешь проверить!'''
     bot.send_message(message.chat.id, text)
 
@@ -30,9 +39,20 @@ def num(message):
     bot.send_message(message.chat.id, num_generation)
 
 
+@bot.message_handler(func=lambda message: check_valid_poll(message.text))
+def get_poll(message):
+    temp = message.text.split("\n")
+    question = temp[0]
+    ans = temp[1:]
+    bot.send_poll(message.chat.id, question, ans)
+
+
 @bot.message_handler(content_types=['text'])
 def echo(message):
     bot.send_message(message.chat.id, message.text * 10)
+
+if __name__ == '__main__':
+    bot.infinity_polling()
 
 if __name__ == '__main__':
     bot.infinity_polling()
