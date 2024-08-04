8b1bbc1b2017698c99a488b0baae4c0d06c83c7b
Siabram1 <146183763+B1ven@users.noreply.github.com>
2024-08-04 15:50:58 +0300

    ДЗ "Различие атрибутов класса и экземпляра"

---
+++ module_5_4.py
@@ -0,0 +1,110 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return object.__new__(cls)
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor: int):
+        self.new_floor = new_floor
+        if self.new_floor <= self.number_of_floors:
+            for i in range(new_floor):
+                print(i + 1)
+        else:
+            print('Нет такого этажа')
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, количество этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        return House(self.name, self.number_of_floors + value)
+
+    def __radd__(self, value):
+        return House(self.name, value + self.number_of_floors)
+
+    def __iadd__(self, value):
+        self.number_of_floors += value
+        return House(self.name, self.number_of_floors)
+
+    def __del__(self):
+        print(f'{self.name} снесён, но он останется в истории')
+
+
+# h1 = House('ЖК Горский', 18)
+# h2 = House('Домик в деревне', 2)
+# h1.go_to(5)
+# h2.go_to(10)
+#
+# h1 = House('ЖК Эльбрус', 10)
+# h2 = House('ЖК Акация', 20)
+#
+# # __str__
+# print(h1)
+# print(h2)
+#
+# # __len__
+# print(len(h1))
+# print(len(h2))
+
+
+# h1 = House('ЖК Эльбрус', 10)
+# h2 = House('ЖК Акация', 20)
+#
+# print(h1)
+# print(h2)
+#
+# print(h1 == h2) # __eq__
+#
+# h1 = h1 + 10 # __add__
+# print(h1)
+# print(h1 == h2)
+#
+# h1 += 10 # __iadd__
+# print(h1)
+#
+# h2 = 10 + h2 # __radd__
+# print(h2)
+#
+# print(h1 > h2) # __gt__
+# print(h1 >= h2) # __ge__
+# print(h1 < h2) # __lt__
+# print(h1 <= h2) # __le__
+# print(h1 != h2) # __ne__
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
