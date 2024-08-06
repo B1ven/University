72997e23b7cb5a97738edba0613a57f97c960350
Siabram1 <146183763+B1ven@users.noreply.github.com>
2024-08-06 17:20:59 +0300

    module_8_1.py

--- module_8_1.py
+++ module_8_1.py
@@ -1,7 +1,7 @@
 def add_everything_up(a,b):
     try:
         return a + b
-    except(TypeError):
+    except TypeError:
         return str(a) + str(b)
         
 print(add_everything_up(123.456, 'строка'))
