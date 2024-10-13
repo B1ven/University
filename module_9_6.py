4538abb30d33feadea2dd3b8ca19d6ab9da17d11
Siabram1 <146183763+B1ven@users.noreply.github.com>
2024-10-13 20:55:02 +0300

    Генератор

--- module_9_6.py
+++ module_9_6.py
@@ -1,9 +1,7 @@
 def all_variants(text):
-    res = []
     for c in range(len(text)):
         for q in range(c + 1, len(text) + 1):
-            res.append(text[c:q])
-    yield '\n'.join(res)
+            yield text[c:q]
 
 
 a = all_variants("abc")
