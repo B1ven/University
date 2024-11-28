0ed74f14aefee4b4dfa8ca1752509042e9bf1b18
Siabram1 <146183763+B1ven@users.noreply.github.com>
2024-11-28 14:25:32 +0300

    Обновление для регистрации

--- crud_function.py
+++ crud_function.py
@@ -13,6 +13,16 @@ def initiate_db():
     price INTEGER NOT NULL)
     """)
 
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    balance INTEGER NOT NULL
+    )
+    """)
+
 
 for i in range(1, 5):
     cursor.execute(f"INSERT INTO Product (title, description, price) VALUES(?,?,?)",
@@ -21,4 +31,8 @@ for i in range(1, 5):
 
 def get_all_products():
     return cursor.execute("SELECT * FROM Product")
+
+
+def user_auth(name, age, email):
+    cursor.execute("INSERT INTO Users (username, email, age) VALUES(?,?,?)", (username, email, age))
 
